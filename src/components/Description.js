ef198b842580a56e38418cff27f4d13cebed7b82
garvbahal <garvbahal75@gmail.com>
2025-01-17 12:58:26 +0530

    fully-responsive

--- src/components/Description.js
+++ src/components/Description.js
@@ -5,11 +5,9 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 const Description = (props) => {
   return (
     <div className="flex flex-col items-center justify-center relative">
-      <div>
-        <img
-          src={props.show.image}
-          className="rounded-full w-[140px] aspect-square object-cover absolute -top-[120px] left-[10px]"
-        />
+      <div className="z-1 rounded-full w-[140px] relative aspect-square md:absolute md:-top-[120px] md:left-[10px] ">
+        <img src={props.show.image} className=" rounded-full absolute z-30 " />
+        <div className="-top-[6px] left-[10px] absolute w-[140px] aspect-square rounded-full bg-violet-500"></div>
       </div>
       <div className="flex flex-col items-center relative">
         <h2 className="font-bold text-lg md:text-2xl capitalize tracking-wider mt-[28px] text-center">
@@ -18,19 +16,32 @@ const Description = (props) => {
         <p className=" text-violet-300 uppercase text-sm mb-[20px] text-center">
           {props.show.job}
         </p>
-        <FaQuoteLeft />
-        <p className="">{props.show.text}</p>
-        <FaQuoteRight />
-        <div>
-          <button onClick={props.leftClickHandler}>
-            <FaChevronLeft />
+        <FaQuoteLeft color="#a78bfa" />
+        <p className=" mt-[16px] mb-[20px] text-center text-slate-500">
+          {props.show.text}
+        </p>
+        <FaQuoteRight color="#a78bfa" />
+        <div className=" mt-[40px] flex gap-5  text-violet-400 text-3xl">
+          <button
+            className=" hover:text-violet-500 transition-all duration-200 "
+            onClick={props.leftClickHandler}
+          >
+            <FaChevronLeft fontSize={20} />
           </button>
 
-          <button onClick={props.rightClickHandler}>
-            <FaChevronRight />
+          <button
+            className=" hover:text-violet-500 transition-all duration-200"
+            onClick={props.rightClickHandler}
+          >
+            <FaChevronRight fontSize={20} />
           </button>
         </div>
-        <button onClick={props.surpriseButton}>Surprise Me</button>
+        <button
+          className="mt-5 bg-violet-400 hover:bg-violet-500 shadow-md cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg transition-all duration-200"
+          onClick={props.surpriseButton}
+        >
+          Surprise Me
+        </button>
       </div>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className=" text-[36px]">
-      <h1 className="text-center leading-[40px]">Our Testimonials</h1>
+      <h1 className="text-center text-4xl font-bold leading-[40px]">Our Testimonials</h1>
       <div className="h-[4px] w-1/2 mx-auto mt-1 bg-[#a78bfa]"></div>
     </div>
   );
